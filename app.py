e1d185abe322c0586568875ec34f9d371162f890
Alessandro Rizzi <gmsccntdenmark@gmail.com>
2025-02-04 15:39:54 +0100

    v 0.0.1 - streamlit with uv setup

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def main():
+    st.title("This is a titile")
+    st.write("# This is a MD title")
+    st.write("_...I think they're the same..._")
+
+if __name__ == "__main__":
+    main()
